512a825e6e249585acab5a28714231e97cf00c10
Marta Nara <marta.nara.gt@gmail.com>
2023-04-15 20:57:05 +0200

    Add action buttons to table

--- src/pages/Issues/Issues.tsx
+++ src/pages/Issues/Issues.tsx
@@ -1,52 +1,64 @@
 import React from "react";
 
+import { TbEdit, TbTrash } from "react-icons/tb";
+
 import { AddIssueModal } from "components/AddIssueModal/AddIssueModal";
 import StatusLabel from "components/StatusLabel";
 import Wrapper from "components/Wrapper";
 
 import { useAppContext } from "context";
-
 import "./styles.scss";
-import SettingsDropdownMenu from "components/SettingsDropdownMenu";
+
 
 const Issues = () => {
-    const { issuesList } = useAppContext();
+    const { issuesList, deleteIssue } = useAppContext();
 
     return (
         <Wrapper>
-            <div className="modal-wrapper"><AddIssueModal /></div>
+            <div className="modal-wrapper">
+                <AddIssueModal />
+            </div>
             {issuesList.length !== 0 ? (
                 <div className="table-wrapper">
                     <div className="table-scroll">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th style={{ width: "20%" }}>Title</th>
-                                <th style={{ width: "15%" }}>Reporter</th>
-                                <th style={{ width: "15%" }}>Created date</th>
-                                <th style={{ width: "30%" }}>Description</th>
-                                <th style={{ width: "20%" }}>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {issuesList.map((item) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <td>{item.title}</td>
-                                        <td>{item.reporter}</td>
-                                        <td>{item.createdDt}</td>
-                                        <td>{item.description}</td>
-                                        <td>
-                                            <StatusLabel status={item.status} />
-                                        </td>
-                                        <td>
-                                            <SettingsDropdownMenu issueId={item.id}/>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "20%" }}>Title</th>
+                                    <th style={{ width: "15%" }}>Reporter</th>
+                                    <th style={{ width: "15%" }}>Created date</th>
+                                    <th style={{ width: "30%" }}>Description</th>
+                                    <th style={{ width: "10%" }}>Status</th>
+                                    <th style={{ width: "5%" }} />
+                                    <th style={{ width: "5%" }} />
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {issuesList.map((item) => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <td>{item.title}</td>
+                                            <td>{item.reporter}</td>
+                                            <td>{item.createdDt}</td>
+                                            <td>{item.description}</td>
+                                            <td>
+                                                <StatusLabel status={item.status} />
+                                            </td>
+                                            <td>
+                                                <div className="action-button" onClick={() => console.log("click")}>
+                                                    <TbEdit />
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div className="action-button" onClick={() => deleteIssue(item.id)}>
+                                                    <TbTrash />
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             ) : (
