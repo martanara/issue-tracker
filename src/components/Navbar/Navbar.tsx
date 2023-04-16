0fb05a69e262ea98c2318c35aea9481720aa2d04
Marta Nara <marta.nara.gt@gmail.com>
2023-04-16 20:29:42 +0200

    Add styling for RWD

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink } from "react-router-dom";
+
+import { HiOutlineX, HiOutlineMenu } from "react-icons/hi";
 
 import "./styles.scss";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
         <div className="navbar">
-            <ul className="navbar-list">
-                <li className="navbar-icon">
-                    <NavLink to="/">Trackly</NavLink>
-                </li>
-            </ul>
-            <ul className="navbar-list">
+            <div className="navbar-icon">
+                <NavLink to="/">Trackly</NavLink>
+            </div>
+            <div className="navbar-toggle" onClick={toggleMenu}>
+                {isOpen ? <HiOutlineX size={20} color="white" /> : <HiOutlineMenu size={20} color="white" />}
+            </div>
+            <ul className={`navbar-list ${isOpen ? "show-menu" : ""}`}>
                 <li>
-                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/">
+                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/" onClick={toggleMenu}>
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/issues">
+                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/issues" onClick={toggleMenu}>
                         Issues
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/about">
+                    <NavLink className={({ isActive }) => (isActive ? "link-active" : "")} to="/about" onClick={toggleMenu}>
                         About
                     </NavLink>
                 </li>
