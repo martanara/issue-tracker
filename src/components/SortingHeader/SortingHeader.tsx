700604131fdc708a941bd1045f2ead275acc918c
Marta Nara <marta.nara.gt@gmail.com>
2023-04-18 16:44:41 +0200

    Add sorting header

---
+++ src/components/SortingHeader/SortingHeader.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import { BiSortDown } from "react-icons/bi";
+import { useAppContext } from "context";
+import { IIssue } from "interfaces";
+
+import "./styles.scss";
+
+interface ISortingHeaderProps {
+    name: string;
+}
+
+const SortingHeader = (props: ISortingHeaderProps) => {
+    const { issuesList, setIssuesList } = useAppContext();
+
+    const [sortColumn, setSortColumn] = useState<string>("");
+    const [isAscending, setIsAscending] = useState<boolean>(false);
+
+    const handleSort = (name: string) => {
+        setSortColumn(name);
+        if (name === sortColumn) setIsAscending(!isAscending);
+
+        let sortBy = "";
+        switch (name) {
+            case "Created date":
+                sortBy = "createdDt";
+                break;
+            default:
+                sortBy = name.toLowerCase();
+                break;
+        }
+        const sortedIssues = [...issuesList].sort((a: IIssue, b: IIssue) => {
+            const x = a[sortBy].toLowerCase();
+            const y = b[sortBy].toLowerCase();
+            if (x < y) {
+                return isAscending ? -1 : 1;
+            }
+            if (x > y) {
+                return isAscending ? 1 : -1;
+            }
+            return 0;
+        });
+
+        setIssuesList(sortedIssues);
+    };
+
+    return (
+        <div className="header-button" onClick={() => handleSort(props.name)}>
+            <div>{props.name}</div>
+            <div className="icon">
+                <BiSortDown size="13px" />
+            </div>
+        </div>
+    );
+};
+
+export default SortingHeader;
--- src/pages/Issues/Issues.tsx
+++ src/pages/Issues/Issues.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Tooltip } from "react-tooltip";
 
 import { AddIssueModal, EditIssueModal } from "components/AddEditIssueModal/AddEditIssueModal";
 import { ConfirmDeleteModal } from "components/ConfirmDeleteModal/ConfirmDeleteModal";
 import StatusDropdownMenu from "components/StatusDropdownMenu";
+import SortingHeader from "components/SortingHeader";
 import Wrapper from "components/Wrapper";
+import { IIssue } from "interfaces";
 
 import { useAppContext } from "context";
 
@@ -14,29 +16,43 @@ import "./styles.scss";
 const Issues = () => {
     const { issuesList } = useAppContext();
 
+    const [issuesToDisplay, setIssuesToDisplay] = useState<IIssue[]>([]);
+
+    useEffect(() => {
+        setIssuesToDisplay(issuesList);
+    }, [issuesList]);
+
     return (
         <Wrapper>
             <div className="modal-wrapper">
                 <AddIssueModal />
             </div>
             <Tooltip id="my-tooltip" className="tooltip" />
-            {issuesList.length !== 0 ? (
+            {issuesToDisplay.length !== 0 ? (
                 <div className="table-wrapper">
                     <div className="table-scroll">
                         <table>
                             <thead>
                                 <tr>
-                                    <th style={{ width: "20%" }}>Title</th>
-                                    <th style={{ width: "15%" }}>Reporter</th>
-                                    <th style={{ width: "15%" }}>Created date</th>
+                                    <th style={{ width: "20%" }}>
+                                        <SortingHeader name="Title" />
+                                    </th>
+                                    <th style={{ width: "15%" }}>
+                                        <SortingHeader name="Reporter" />
+                                    </th>
+                                    <th style={{ width: "15%" }}>
+                                        <SortingHeader name="Created date" />
+                                    </th>
                                     <th style={{ width: "30%" }}>Description</th>
-                                    <th style={{ width: "10%" }}>Status</th>
+                                    <th style={{ width: "10%" }}>
+                                        <SortingHeader name="Status" />
+                                    </th>
                                     <th style={{ width: "5%" }} />
                                     <th style={{ width: "5%" }} />
                                 </tr>
                             </thead>
                             <tbody>
-                                {issuesList.map((issue) => {
+                                {issuesToDisplay.map((issue) => {
                                     return (
                                         <tr key={issue.id}>
                                             <td>{issue.title}</td>
